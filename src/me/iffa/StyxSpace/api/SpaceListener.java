9fdc46faec3c0e9afccff1aac03c0395c06cf978
Santtu Elo <iffamies1337@hotmail.fi>
2011-07-20 11:33:00 +0300

    iffa: New events, portal & entity related

--- src/me/iffa/StyxSpace/api/SpaceListener.java
+++ src/me/iffa/StyxSpace/api/SpaceListener.java
@@ -1,6 +1,7 @@
 package me.iffa.styxspace.api;
 
 import org.bukkit.Location;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 
 import me.iffa.styxspace.StyxSpace;
@@ -65,11 +66,44 @@ public class SpaceListener {
 	}
 
 	/**
-	 * Called when a player uses the space command
+	 * Called when a player creates a sign portal.
+	 * 
+	 * @param player
+	 *            Player that created a sign portal
+	 * @param portalname
+	 *            Name of the portal created
+	 */
+	public void onPortalCreate(Player player, String portalname) {
+	}
+
+	/**
+	 * Called when a player destroys/removes a sign portal.
+	 * 
+	 * @param player
+	 *            Player that removed a sign portal
+	 * @param portalname
+	 *            Name of the portal removed
+	 */
+	public void onPortalRemove(Player player, String portalname) {
+	}
+
+	/**
+	 * Called when a player uses the space command.
 	 * 
 	 * @param player
 	 *            Player that used the space command
 	 */
 	public void onSpaceCommand(Player player) {
+	}
+
+	/**
+	 * Called when an entity is not allowed to spawn in space.
+	 * 
+	 * @param entity
+	 *            Entity that wasn't allowed to spawn
+	 * @param isHostile
+	 *            If the entity was hostile
+	 */
+	public void onAntiMobSpawn(Entity entity, boolean isHostile) {
 	}
 }
